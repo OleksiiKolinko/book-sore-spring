5a237f9a27ab293fe846cd2693859284c79afc47
Oleksii Kolinko <138433107+OleksiiKolinko@users.noreply.github.com>
2023-10-31 00:01:19 +0100

    HW34-solution added changes

---
+++ src/test/java/mate/academy/BookStoreSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package mate.academy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookStoreSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
